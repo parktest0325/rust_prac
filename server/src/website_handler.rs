b6fc795e4761480ca46db15385b186000baf53f3
dongkim <parktest0325@gmail.com>
2024-02-18 23:14:26 +0900

    fix: Path Traversal

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -12,7 +12,18 @@ impl WebsiteHandler {
     }
     fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
-        fs::read_to_string(path).ok()
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                }
+                else {
+                    println!("Directory Traversal Attack Attempted: {}", file_path);
+                    None
+                }
+            },
+            Err(_) => None,
+        }
     }
 }
 
@@ -23,6 +34,7 @@ impl Handler for WebsiteHandler {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
                 "/test" => Response::new(StatusCode::Ok, self.read_file("test.html")),
+                // 모든 정적 파일을 매핑하기 위해 추가한 이 코드때문에 Path Traversal 취약점이 발생할 수 있다
                 path => match self.read_file(path) {
                     Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
                     None => Response::new(StatusCode::NotFound, None)
