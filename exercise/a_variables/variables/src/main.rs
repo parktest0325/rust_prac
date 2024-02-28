60789f7af50a5af708d7b36d16e4319eb568b0a7
dongkim <parktest0325@gmail.com>
2024-02-29 01:35:05 +0900

    a_variables - Extra

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -5,6 +5,11 @@ fn main() {
     let (mut missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
     println!("Firing {} of my {} missiles...", ready, missiles);
 
-    missiles = missiles - ready;
+    missiles = missiles - ready;    // 지우고나면 variable does not need to be mutable 에러가 발생
     println!("{} missiles left", missiles);
+    
+    // READY_AMOUNT = 1; // cannot assign to this expression
+
+    let ext: i32;
+
 }
